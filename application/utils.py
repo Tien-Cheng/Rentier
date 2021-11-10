c3da81b2757573d02b84ac560386e1a27cf692b7
Tien Cheng Oh <2b01.2012072.OhTienCheng@gmail.com>
2021-11-10 17:49:36 +0800

    [refactor] Formatted all files

--- application/utils.py
+++ application/utils.py
@@ -2,11 +2,13 @@ from functools import wraps
 from flask import redirect, request, url_for, session
 
 
-def login_required(func): 
-    """ Decorator to check if user is logged in . """
+def login_required(func):
+    """Decorator to check if user is logged in ."""
+
     @wraps(func)
     def decorated_func(*args, **kwargs):
-        if session.get("user_id") is None: # Check if user is logged in
+        if session.get("user_id") is None:  # Check if user is logged in
             return redirect(url_for("login"), next=request.url)
         return func(*args, **kwargs)
+
     return decorated_func
