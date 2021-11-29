32bfa658e346ba4651967c112c38c61f4c761512
Tien Cheng Oh <2b01.2012072.OhTienCheng@gmail.com>
2021-11-29 21:39:36 +0800

    [config] Add test config and removed pydotenv

--- application/__init__.py
+++ application/__init__.py
@@ -1,9 +1,17 @@
 from flask import Flask
+import os
 # Persistent Storage
 from flask_sqlalchemy import SQLAlchemy
 import joblib
 app = Flask(__name__)
-app.config.from_pyfile("dev_config.cfg")
+
+if "TESTING" in os.environ:
+    app.config.from_envvar("TESTING")
+    print("Using config for TESTING")
+elif "DEVELOPMENT" in os.environ:
+    app.config.from_envvar("DEVELOPMENT")
+    print("Using config for DEVELOPMENT")
+
 db = SQLAlchemy(app)
 model_path = "./application/static/ai_model_2.joblib"
 ai_model = joblib.load(model_path)
