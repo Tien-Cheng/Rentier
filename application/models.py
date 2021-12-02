203cee5dd38586d4e8dd990e42c42235a9e4b9d5
Tien Cheng Oh <2b01.2012072.OhTienCheng@gmail.com>
2021-12-02 16:59:54 +0800

    [fix] Fix link column accidently set to NOT NULL

--- application/models.py
+++ application/models.py
@@ -8,6 +8,10 @@ from sqlalchemy.exc import IntegrityError
 
 
 class Entry(db.Model):
+    """
+    Entry class represents a prediction made and the features entered by a user
+    """
+
     __tablename__ = "history"
 
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
@@ -22,7 +26,7 @@ class Entry(db.Model):
     elevator = db.Column(db.Boolean, nullable=False)
     pool = db.Column(db.Boolean, nullable=False)
     actual_price = db.Column(db.Float, nullable=True)
-    link = db.Column(db.String(500), nullable=False)
+    link = db.Column(db.String(500), nullable=True)
     prediction = db.Column(db.Float, nullable=False)
     created = db.Column(db.DateTime, nullable=False)
 
@@ -37,7 +41,10 @@ class Entry(db.Model):
 
     @validates("bathrooms")
     def validate_bathrooms(self, key, bathrooms):
-        assert type(bathrooms) in {int, float}, "Bathrooms should be an integer or float"
+        assert type(bathrooms) in {
+            int,
+            float,
+        }, "Bathrooms should be an integer or float"
         assert bathrooms >= 0, "Bathrooms should be greater than or equal to 0"
         return bathrooms
 
@@ -54,7 +61,6 @@ class Entry(db.Model):
         ), "Minimum number of lights should be an integer"
         assert minimum_nights >= 0, "Minimum number of lights should not be negative"
         return minimum_nights
-
 
     @validates("wifi")
     def validate_wifi(self, key, wifi):
@@ -74,25 +80,35 @@ class Entry(db.Model):
     @validates("neighborhood")
     def validate_neighborhood(self, key, neighborhood):
         assert type(neighborhood) is str, "Data type should be a string"
-        assert neighborhood in NEIGHBORHOODS, "Neighborhood should be one of the recognized neighborhoods"
+        assert (
+            neighborhood in NEIGHBORHOODS
+        ), "Neighborhood should be one of the recognized neighborhoods"
         return neighborhood
 
     @validates("room_type")
     def validate_room_type(self, key, room_type):
         assert type(room_type) is str, "Data type should be a string"
-        assert room_type in ROOM_TYPES, "Room Type should be one of the valid Room Types"
+        assert (
+            room_type in ROOM_TYPES
+        ), "Room Type should be one of the valid Room Types"
         return room_type
-    
+
     @validates("actual_price")
     def validate_actual_price(self, key, actual_price):
-        assert type(actual_price) in {type(None), int, float}, "Either actual price should be None, int or float"
+        assert type(actual_price) in {
+            type(None),
+            int,
+            float,
+        }, "Either actual price should be None, int or float"
         if actual_price is not None:
-            assert actual_price >= 0, "Actual price should be greater than or equal to 0"
+            assert (
+                actual_price >= 0
+            ), "Actual price should be greater than or equal to 0"
         return actual_price
 
     @validates("prediction")
     def validate_prediction(self, key, prediction):
-        assert type(prediction) is float , "Data type should be a float or int"
+        assert type(prediction) in {float, int}, "Data type should be a float or int"
         assert prediction > 0, "Prediction should be positive"
         return prediction
 
@@ -100,7 +116,16 @@ class Entry(db.Model):
     def validates_created(self, key, created):
         assert type(created) is dt.datetime, "created should be a datetime object"
         return created
-    
+
+    @validates("link")
+    def validates_link(self, key, link):
+        assert type(link) in {type(None), str}, "Link should be None or Str"
+        return link
+
+
+# TODO: Standarize, then have specific exceptions like integrity error be handled in routes
+
+
 def add_entry(entry):
     try:
         db.session.add(entry)
@@ -111,6 +136,7 @@ def add_entry(entry):
         flash(str(error), "danger")
         abort(500)
 
+
 def delete_entry(entry):
     try:
         db.session.delete(entry)
@@ -119,13 +145,13 @@ def delete_entry(entry):
         db.session.rollback()
         abort(500)
 
+
 def get_history(user_id):
     try:
         return db.session.query(Entry).filter_by(user_id=user_id).all()
     except Exception as error:
         flash(str(error), "danger")
         abort(500)
-
 
 
 class User(db.Model):
@@ -151,18 +177,23 @@ class User(db.Model):
         Validate email address. Email address should already have been validated by the Flask Form, but we double check the input here just in case
         """
         assert len(email) <= 255, "Email address should be at most 255 characters long."
-        assert re.search(r"^[a-z0-9]+[\._]?[a-z0-9]+[@]\w+[.]\w{2,3}$", email) is not None, "Email address is invalid"
+        assert (
+            re.search(r"^[a-z0-9]+[\._]?[a-z0-9]+[@]\w+[.]\w{2,3}$", email) is not None
+        ), "Email address is invalid"
         return email
 
     @validates("password_hash")
     def validate_hash(self, key, password_hash):
-        assert len(password_hash) >= 64, "Password hash should be at least 64 characters long."
+        assert (
+            len(password_hash) >= 64
+        ), "Password hash should be at least 64 characters long."
         return password_hash
 
     @validates("created")
     def validates_created(self, key, created):
         assert type(created) is dt.datetime, "created should be a datetime object"
         return created
+
 
 def add_user(new_user):
     try:
