c808b3719db9e308484951700da7203ee5010bd1
Tien Cheng Oh <2b01.2012072.OhTienCheng@gmail.com>
2021-12-02 17:00:27 +0800

    [refactor] Removed commented out code

--- tests/test_Entry.py
+++ tests/test_Entry.py
@@ -90,34 +90,8 @@ def test_EntryClass(entrylist, capsys):
 3. Out of range input
 """
 
-### TODO: Move to api testing, as not possible to test here
-# @pytest.mark.xfail(reason="User referenced in Entry does not exist")
-# @pytest.mark.parametrize(
-#     "entrylist",
-#     [
-#         [
-#             0,  # User id 0 should not exist
-#             1,
-#             1,
-#             2,
-#             90,
-#             "Private room",
-#             "Bukit Timah",
-#             True,
-#             True,
-#             True,
-#             80,
-#             "https://www.airbnb.com/rooms/71896",  # Test optional
-#             70.83,
-#         ],
-#         [None, 2, 1, 4, 10, "Private room", "Ang Mo Kio", 1, 0, 1, 60, None, 54.5],
-#     ],
-# )
-# def test_EntryValidation_UserDoesNotExist(entrylist, capsys):
-#     test_EntryClass(entrylist, capsys)
 
-
-@pytest.mark.xfail(reason="Invalid Data Type")
+@pytest.mark.xfail(reason="Invalid Data Type", strict=True)
 @pytest.mark.parametrize(
     "entrylist",
     [
@@ -342,7 +316,7 @@ def test_EntryValidation_InvalidDataType(entrylist, capsys):
     #     assert type(entrylist[12]) is float, "Prediction should be a float"
 
 
-@pytest.mark.xfail(reason="Inputs out of range")
+@pytest.mark.xfail(reason="Inputs out of range", strict=True)
 @pytest.mark.parametrize(
     "entrylist",
     [
@@ -481,7 +455,7 @@ def test_EntryValidation_RangeTest(entrylist, capsys):
     test_EntryClass(entrylist, capsys)
 
 
-@pytest.mark.xfail(reason="Missing inpputs")
+@pytest.mark.xfail(reason="Missing inputs", strict=True)
 @pytest.mark.parametrize(
     "entrylist",
     [
@@ -520,3 +494,6 @@ def test_EntryValidation_RangeTest(entrylist, capsys):
 )
 def test_EntryValidationMissing(entrylist, capsys):
     test_EntryClass(entrylist, capsys)
+
+
+# TODO: Consistency Tests: Check that accomodation > #beds
