4b23d621291718cb31f6ce4646228f424a65f219
Tien Cheng Oh <2b01.2012072.OhTienCheng@gmail.com>
2021-12-02 17:58:35 +0800

    [test] Add tests for add and get history api

---
+++ tests/test_HistoryAPI.py
@@ -0,0 +1,159 @@
+import pytest
+import json
+from application import app
+
+# Add Entry
+
+# Test that adding normal entries to history works correctly
+@pytest.mark.usefixtures("populate_users")
+@pytest.mark.parametrize(
+    "entrylist",
+    [
+        [
+            2,  # User ID
+            3,  # Beds
+            1,  # Bathrooms
+            6,  # Accomodates
+            90,  # Minimum Nights
+            "Private room",  # Room type
+            "Tampines",  # Neighborhood
+            True,  # Wifi
+            False,  # Elevator
+            False,  # Pool
+            178,  # Actual price
+            "https://www.airbnb.com/rooms/71609",  # Link to listing
+            155.06,  # Predicted
+        ]
+    ],
+)
+def test_add_entry(client, entrylist, capsys):
+    with capsys.disabled():
+        data = dict(
+            beds=entrylist[1],
+            bathrooms=entrylist[2],
+            accomodates=entrylist[3],
+            minimum_nights=entrylist[4],
+            room_type=entrylist[5],
+            neighborhood=entrylist[6],
+            wifi=entrylist[7],
+            elevator=entrylist[8],
+            pool=entrylist[9],
+            actual_price=entrylist[10],
+            link=entrylist[11],
+            prediction=entrylist[12],
+        )
+
+        response = client.post(
+            f"/api/history/{entrylist[0]}",
+            data=json.dumps(data),
+            content_type="application/json",
+        )
+        response_body = json.loads(response.get_data(as_text=True))
+
+        assert response.status_code == 200
+        assert response.headers["Content-Type"] == "application/json"
+
+        assert response_body["result"]
+
+
+# Test that adding entries with invalid user_id results in failure
+@pytest.mark.skipif(
+    app.config["SQLALCHEMY_DATABASE_URI"].startswith("sqlite"),
+    reason="SQLite does not enforce foreign key constraints",
+)
+@pytest.mark.usefixtures("populate_users")
+@pytest.mark.xfail(reason="User ID does not exist", strict=True)
+@pytest.mark.parametrize(
+    "entrylist",
+    [
+        [
+            -1,  # User_id
+            2,  # Beds
+            1,  # Bathrooms
+            2,  # Accomodates
+            7,  # Minimum Nights
+            "Private room",  # Room type
+            "Ang Mo Kio",  # Neighborhood
+            True,  # Wifi
+            True,  # Elevator
+            False,  # Pool
+            None,  # Actual Price
+            None,  # Link
+            94.0,  # Prediction
+        ],
+        [
+            545,
+            1,
+            1,
+            2,
+            90,
+            "Private room",
+            "Bukit Timah",
+            True,
+            True,
+            True,
+            80,
+            "https://www.airbnb.com/rooms/71896",
+            70.83,
+        ],
+    ],
+)
+def test_add_entry_invalid_user_id(client, entrylist, capsys):
+    test_add_entry(client, entrylist, capsys)
+
+
+# Tests on validity of data are conducted in test_Entry.py, so will not duplicate here
+
+
+# Get History
+@pytest.mark.parametrize(
+    "user_history",
+    [
+        {
+            "user_id": 2,
+            "history": [
+                {
+                    "beds": 3,
+                    "bathrooms": 1,
+                    "accomodates": 6,
+                    "minimum_nights": 90,
+                    "room_type": "Private room",
+                    "neighborhood": "Tampines",
+                    "wifi": True,
+                    "elevator": False,
+                    "pool": False,
+                    "actual_price": 178,
+                    "link": "https://www.airbnb.com/rooms/71609",
+                    "prediction": 155.06,
+                }
+            ],
+        },
+    ],
+)
+def test_get_user_history(client, user_history, capsys):
+    with capsys.disabled():
+        user_id = user_history["user_id"]
+        response = client.get(f"/api/history/{user_id}")
+        response_body = json.loads(response.get_data(as_text=True))
+
+        assert response.status_code == 200
+        assert response.headers["Content-Type"] == "application/json"
+        for entry, expected in zip(response_body, user_history["history"]):
+            for key in (
+                "beds",
+                "bathrooms",
+                "accomodates",
+                "minimum_nights",
+                "room_type",
+                "neighborhood",
+                "wifi",
+                "elevator",
+                "pool",
+                "actual_price",
+                "link",
+                "prediction",
+            ):
+                assert entry[key] == expected[key]
+
+
+# Remove Entry
