6b9defc871a36974c2383ebb850d3ce583ad3cd9
Tien Cheng Oh <2b01.2012072.OhTienCheng@gmail.com>
2021-12-03 16:53:54 +0800

    [feat] Add Jinja filter for custom date rendering

--- application/utils.py
+++ application/utils.py
@@ -1,5 +1,6 @@
 from functools import wraps
 from flask import redirect, request, url_for, session, flash
+from application import app
 
 def login_required(func):
     """Flask decorator to check if a user is logged in, else, redirect them to login page, which will direct them back to the original page once logged in. Checks if the secured session cookie contains a user id.
@@ -26,3 +27,7 @@ class API_Error(Exception):
         super().__init__()
         self.message = message
         self.status_code = status_code
+
+@app.template_filter("render_date")
+def render_date(date, format="%H:%M / %d-%m-%Y"):
+    return date.strftime(format)
