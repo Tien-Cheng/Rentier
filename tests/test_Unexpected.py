df4b7444f836732024ed2c407bfdc5a3b9f1c9e2
Tien Cheng Oh <2b01.2012072.OhTienCheng@gmail.com>
2021-12-03 11:14:29 +0800

    [test] Add tests to test endpoints

---
+++ tests/test_Unexpected.py
@@ -0,0 +1,26 @@
+import pytest
+import json
+
+@pytest.mark.parametrize("endpoint", [
+    ("invalid_link", 404),
+    ("history", 303),
+    ("", 200),
+    ("login", 200),
+    ("register", 200),
+    ("predict", 303) # Trying to access page when not logged in
+])
+def test_Route(client, endpoint, capsys):
+    with capsys.disabled():
+        endpoint, code = endpoint[0], endpoint[1]
+        response = client.get(f"/{endpoint}")
+        assert response.status_code == code
+
+@pytest.mark.parametrize("endpoint", [
+    ("history", 200),
+    ("predict", 200)
+])
+@pytest.mark.usefixtures("fake_login")
+def test_Route_Authorized(client, endpoint, capsys):
+    test_Route(client, endpoint, capsys)
+    with client.session_transaction() as sess:
+        sess.pop("user_id", None) # logout
